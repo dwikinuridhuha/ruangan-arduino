bbc6fea118fb8a07dce2581759eb81c89188db81
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2017-11-14 20:38:13 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com> menambahkan kodingan LCD

--- PIR.ino
+++ PIR.ino
@@ -1,37 +1,55 @@
+//Variabel PIR
 int ledPin = 13;                // Memilih pin untuk LED
 int inputPin = 2;               // Memilih input pin (untuk sensor PIR)
-int pirState = LOW;             // Kita mulai, dengan asumsi tidak ada gerakan terdeteksi
+int pirState = LOW;             // Mengasumsi tidak ada gerakan terdeteksi
 int val = 0;                    // Variabel untuk membaca status pin
-int jumlah = 0;
+int jumlah = 0;                 // variabel untuk menghitung masuk ornag
+
+//variabel LED
+//The circuit:
+// * LCD RS pin to digital pin 12
+// * LCD Enable pin to digital pin 11
+// * LCD D4 pin to digital pin 5
+// * LCD D5 pin to digital pin 4
+// * LCD D6 pin to digital pin 3
+// * LCD D7 pin to digital pin 2
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   pinMode(ledPin, OUTPUT);      // Mendeklarasikan LED sebagai output
   pinMode(inputPin, INPUT);     // Mendeklarasikan sensor sebagai input
+  
+  lcd.begin(16, 2);             // Setting awal nomor colum dan baris pada LCD
+  lcd.print("Orang = ");        // Menampilkan pesan orang
+  
   Serial.begin(9600);
 }
 
 void loop(){
-  val = digitalRead(inputPin);  // Membaca nilai masukan
-  if (val == HIGH) {            // Memeriksa apakah input adalah TINGGI
-    digitalWrite(ledPin, HIGH); // Hidupkan LED ON
-//    delay(0);
+  lcd.setCursor(0, 1);                      // Memulai LCD
+  val = digitalRead(inputPin);              // Membaca nilai masukan
+  
+  if (val == HIGH) {                        // Memeriksa apakah input adalah TINGGI
+      digitalWrite(ledPin, HIGH);           // Hidupkan LED ON
    
-    if (pirState == LOW) {
-      jumlah++;
-      // Kita baru saja diaktifkan
-      Serial.print("Motion detected! ");
-      Serial.println(jumlah);
-      // Kami hanya ingin mencetak pada perubahan output, tidak menyatakan
-      pirState = HIGH;
-    }
+      if (pirState == LOW) {                // cek asumsi tidak ada gerakan
+          jumlah++;
+          
+          Serial.print("Motion detected! "); // mencetak pada perubahan output, tidak menyatakan
+          Serial.println(jumlah);            // menampilkan pada konsole
+          lcd.print(jumlah);                 // menampilkan pada LCD
+          
+          pirState = HIGH;                  // di asumsikan ada gerakan terdeteksi
+      }
   } else {
-      digitalWrite(ledPin, LOW); // turn LED OFF
-//      delay(0);   
-      if (pirState == HIGH){
-      // Kita baru saja dimatikan
-      Serial.println("Motion ended!");
-      // Kami hanya ingin mencetak pada perubahan output, tidak menyatakan
-      pirState = LOW;
+      digitalWrite(ledPin, LOW);           // turn LED OFF
+
+      if (pirState == HIGH) {              // cek asumsi ada gerakan
+          
+          Serial.println("Motion ended!"); // mencetak pada perubahan output, tidak ada orang
+          
+          pirState = LOW;                  // di asumsikan tidak ada gerakan terdeteksi
     }
   }
 }
